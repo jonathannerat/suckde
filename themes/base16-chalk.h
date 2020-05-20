5fd023123fd091f70cb8bf76b55072b48432a3a3
Jonathan Teran <jonathan.nerat@gmail.com>
2020-05-20 12:12:01 -0300

    add base16-chalk theme

---
+++ themes/base16-chalk.h
@@ -0,0 +1,31 @@
+/*
+ * Base16: https://github.com/chriskempson/base16
+ */
+
+#define THEME_BACKGROUND "#151515"
+#define THEME_FOREGROUND "#d0d0d0"
+
+/*
+ * Colors
+ */
+#define THEME_BLACK "#151515"
+#define THEME_RED "#fb9fb1"
+#define THEME_GREEN "#acc267"
+#define THEME_YELLOW "#ddb26f"
+#define THEME_BLUE "#6fc2ef"
+#define THEME_MAGENTA "#e1a3ee"
+#define THEME_CYAN "#12cfc0"
+#define THEME_WHITE "#d0d0d0"
+
+/*
+ * Bright colors
+ */
+
+#define THEME_BRIGHTBLACK "#505050"
+#define THEME_BRIGHTRED "#fb9fb1"
+#define THEME_BRIGHTGREEN "#acc267"
+#define THEME_BRIGHTYELLOW "#ddb26f"
+#define THEME_BRIGHTBLUE "#6fc2ef"
+#define THEME_BRIGHTMAGENTA "#e1a3ee"
+#define THEME_BRIGHTCYAN "#12cfc0"
+#define THEME_BRIGHTWHITE "#f5f5f5"
