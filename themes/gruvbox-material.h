a124eb33637abaf3862c48c185975b23925663b5
Jonathan Teran <jonathan.nerat@gmail.com>
2021-06-13 21:33:53 -0300

    add gruvbox-material theme

---
+++ themes/gruvbox-material.h
@@ -0,0 +1,27 @@
+#define THEME_BACKGROUND "#1d2021"
+#define THEME_FOREGROUND "#d4be98"
+
+/*
+ * Colors
+ */
+#define THEME_BLACK   THEME_BACKGROUND
+#define THEME_RED     "#ea6962"
+#define THEME_GREEN   "#a9b665"
+#define THEME_YELLOW  "#d8a657"
+#define THEME_BLUE    "#7daea3"
+#define THEME_MAGENTA "#d3869b"
+#define THEME_CYAN    "#89b482"
+#define THEME_WHITE   "#d4be98"
+
+/*
+ * Bright colors
+ */
+
+#define THEME_BRIGHTBLACK   "#928374"
+#define THEME_BRIGHTRED     "#ef938e"
+#define THEME_BRIGHTGREEN   "#bbc585"
+#define THEME_BRIGHTYELLOW  "#e1bb7e"
+#define THEME_BRIGHTBLUE    "#9dc2ba"
+#define THEME_BRIGHTMAGENTA "#e1acbb"
+#define THEME_BRIGHTCYAN    "#a7c7a2"
+#define THEME_BRIGHTWHITE   "#e2d3ba"
