0360ad4119a4917f7dccf018a17006ca6e2ba89f
Jonathan Teran <jonathan.nerat@gmail.com>
2021-07-05 14:14:22 -0300

    add folke-tokyonight theme
    - https://github.com/folke/tokyonight.nvim

---
+++ themes/folke-tokyonight.h
@@ -0,0 +1,30 @@
+
+#define THEME_BACKGROUND "#1a1b26"
+#define THEME_FOREGROUND "#c0caf5"
+
+/*
+ * Colors
+ */
+#define THEME_BLACK   "#15161e"
+#define THEME_RED     "#f7768e"
+#define THEME_GREEN   "#9ece6a"
+#define THEME_YELLOW  "#e0af68"
+#define THEME_BLUE    "#7aa2f7"
+#define THEME_MAGENTA "#bb9af7"
+#define THEME_CYAN    "#7dcfff"
+#define THEME_WHITE   "#a9b1d6"
+
+/*
+ * Bright colors
+ */
+
+#define THEME_BRIGHTBLACK   "#414868"
+#define THEME_BRIGHTRED     "#f7768e"
+#define THEME_BRIGHTGREEN   "#9ece6a"
+#define THEME_BRIGHTYELLOW  "#e0af68"
+#define THEME_BRIGHTBLUE    "#7aa2f7"
+#define THEME_BRIGHTMAGENTA "#bb9af7"
+#define THEME_BRIGHTCYAN    "#7dcfff"
+#define THEME_BRIGHTWHITE   "#c0caf5"
+
+#define THEME_EXTRA_COLORS  [16] = "#ff9e64", [17] = "#db4b4b"
