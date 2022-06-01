9032e061d4565845d6aba98320a12337639d4c64
Jonathan Teran <jonathan.nerat@gmail.com>
2022-05-31 22:29:31 -0300

    add material_ocean theme

---
+++ themes/material_ocean.h
@@ -0,0 +1,31 @@
+/*
+ * Base16: https://github.com/chriskempson/base16
+ */
+
+#define THEME_BACKGROUND "#0F111A"
+#define THEME_FOREGROUND "#E5E9F0"
+
+/*
+ * Colors
+ */
+#define THEME_BLACK   "#0F111A"
+#define THEME_RED     "#BF616A"
+#define THEME_GREEN   "#A3BE8C"
+#define THEME_YELLOW  "#EBCB8B"
+#define THEME_BLUE    "#81A1C1"
+#define THEME_MAGENTA "#B48EAD"
+#define THEME_CYAN    "#88C0D0"
+#define THEME_WHITE   "#E5E9F0"
+
+/*
+ * Bright colors
+ */
+
+#define THEME_BRIGHTBLACK   "#4C566A"
+#define THEME_BRIGHTRED     "#BF616A"
+#define THEME_BRIGHTGREEN   "#A3BE8C"
+#define THEME_BRIGHTYELLOW  "#EBCB8B"
+#define THEME_BRIGHTBLUE    "#81A1C1"
+#define THEME_BRIGHTMAGENTA "#B48EAD"
+#define THEME_BRIGHTCYAN    "#88C0D0"
+#define THEME_BRIGHTWHITE   "#E5E9F0"
