49da06e5fce8ca336d41bcd55c23984556a2299e
Jonathan Teran <jonathan.nerat@gmail.com>
2024-08-18 15:33:47 -0300

    themes: add catppuccin, gruvbox-flat themes

---
+++ themes/catppuccin.h
@@ -0,0 +1,31 @@
+/*
+ * Base16: https://github.com/chriskempson/base16
+ */
+
+#define THEME_BACKGROUND "#1E1E2E"
+#define THEME_FOREGROUND "#E5E9F0"
+
+/*
+ * Colors
+ */
+#define THEME_BLACK   "#1E1E2E"
+#define THEME_RED     "#F38BA8"
+#define THEME_GREEN   "#A6E3A1"
+#define THEME_YELLOW  "#F9E2AF"
+#define THEME_BLUE    "#89B4FA"
+#define THEME_MAGENTA "#F5C2E7"
+#define THEME_CYAN    "#94E2D5"
+#define THEME_WHITE   "#BAC2DE"
+
+/*
+ * Bright colors
+ */
+
+#define THEME_BRIGHTBLACK   "#585B70"
+#define THEME_BRIGHTRED     "#F38BA8"
+#define THEME_BRIGHTGREEN   "#A6E3A1"
+#define THEME_BRIGHTYELLOW  "#F9E2AF"
+#define THEME_BRIGHTBLUE    "#89B4FA"
+#define THEME_BRIGHTMAGENTA "#F5C2E7"
+#define THEME_BRIGHTCYAN    "#94E2D5"
+#define THEME_BRIGHTWHITE   "#A6ADC8"
---
+++ themes/gruvbox-flat.h
@@ -0,0 +1,27 @@
+#define THEME_BACKGROUND "#1d2021"
+#define THEME_FOREGROUND "#d4be98"
+
+/*
+ * Colors
+ */
+#define THEME_BLACK   "#1D2021"
+#define THEME_RED     "#ea6962"
+#define THEME_GREEN   "#a9b665"
+#define THEME_YELLOW  "#d8a657"
+#define THEME_BLUE    "#7daea3"
+#define THEME_MAGENTA "#d3869b"
+#define THEME_CYAN    "#89b482"
+#define THEME_WHITE   "#d4be98"
+
+/*
+ * Bright colors
+ */
+
+#define THEME_BRIGHTBLACK   "#17191a"
+#define THEME_BRIGHTRED     "#ea6962"
+#define THEME_BRIGHTGREEN   "#a9b665"
+#define THEME_BRIGHTYELLOW  "#d8a657"
+#define THEME_BRIGHTBLUE    "#7daea3"
+#define THEME_BRIGHTMAGENTA "#d3869b"
+#define THEME_BRIGHTCYAN    "#89b482"
+#define THEME_BRIGHTWHITE   "#d4be98"
